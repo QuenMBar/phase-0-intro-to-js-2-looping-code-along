dc6e11e8e248e533303a1e1d4c72b4c9ed6d6388
QuenMBar <Qmb2@students.calvin.edu>
2021-02-10 14:14:00 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, reason) {
+    let responses = [];
+    names.forEach((name) => {
+        responses.push(`Thank you, ${name}, for the wonderful ${reason} gift!`);
+    });
+    // Other for loop
+    // for (let i = 0; index < names.length; i++) {
+    //     responses.push(`Thank you, ${names[i]}, for the wonderful ${reason} gift!`);
+    // }
+    return responses;
+}
+
+function countDown(countDownNum) {
+    while (countDownNum >= 0) {
+        console.log(countDownNum--);
+    }
+}
